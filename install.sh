75dd00f760b58c1a2f4102e28f86ec0886355b2f
Jonathan Fuerst <jonf@itu.dk>
2016-04-19 14:55:31 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+sh <(curl https://j.mp/spf13-vim3 -L)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-brew install reattach-to-user-namespace
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/ctags ~/ctags
-ln -s ~/dotfiles/slate.js ~/.slate.js
 ln -s ~/dotfiles/zpreztorc ~/.zpreztorc
 ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/hammerspoon ~/.hammerspoon
+ln -s ~/dotfiles/ctags ~/ctags
+ln -s ~/dotfiles/vim/gvimrc.local ~/.gvimrc.local
+ln -s ~/dotfiles/vim/vimrc.before.local ~/.vimrc.before.local
+ln -s ~/dotfiles/vim/vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/dotfiles/vim/vimrc.local ~/.vimrc.local
+if [ "$(uname)" == "Darwin"  ]; then
+    brew update
+    brew install reattach-to-user-namespace
+    ln -s ~/dotfiles/hammerspoon ~/.hammerspoon
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux"  ]; then
+    echo "linux"
+fi
+vim +BundleInstall! +BundleClean +q
