e6d29f9c3e2d0373f55b27f8e9bbc88497597c59
Jonathan Furst <jonf@itu.dk>
2016-05-24 02:28:29 -0700

    moved symbolic links ti begin

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/ctags ~/ctags
+ln -s ~/dotfiles/zpreztorc ~/.zpreztorc
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/ctags ~/ctags
+ln -s ~/dotfiles/vim/gvimrc.local ~/.gvimrc.local
+ln -s ~/dotfiles/vim/vimrc.before.local ~/.vimrc.before.local
+ln -s ~/dotfiles/vim/vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/dotfiles/vim/vimrc.local ~/.vimrc.local
 if [ "$(uname)" == "Darwin"  ]; then
     brew update
     brew install zsh
@@ -19,13 +28,4 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 done
 sh <(curl https://j.mp/spf13-vim3 -L)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/ctags ~/ctags
-ln -s ~/dotfiles/zpreztorc ~/.zpreztorc
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/ctags ~/ctags
-ln -s ~/dotfiles/vim/gvimrc.local ~/.gvimrc.local
-ln -s ~/dotfiles/vim/vimrc.before.local ~/.vimrc.before.local
-ln -s ~/dotfiles/vim/vimrc.bundles.local ~/.vimrc.bundles.local
-ln -s ~/dotfiles/vim/vimrc.local ~/.vimrc.local
 vim +BundleInstall! +BundleClean +q
