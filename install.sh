630b23d913880c120fb7c18b7e815adfe0a0b774
Jonathan Fuerst <jonf@itu.dk>
2016-10-06 10:17:45 +0200

    linux adjustments

--- install.sh
+++ install.sh
@@ -4,14 +4,12 @@ ln -s ~/dotfiles/ctags ~/ctags
 ln -s ~/dotfiles/zpreztorc ~/.zpreztorc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/ctags ~/ctags
-ln -s ~/dotfiles/vim/gvimrc.local ~/.gvimrc.local
-ln -s ~/dotfiles/vim/vimrc.before.local ~/.vimrc.before.local
-ln -s ~/dotfiles/vim/vimrc.bundles.local ~/.vimrc.bundles.local
-ln -s ~/dotfiles/vim/vimrc.local ~/.vimrc.local
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 if [ "$(uname)" == "Darwin"  ]; then
     brew update
     brew install zsh
     brew install macvim --with-lua
+    brew install neovim/neovim/neovim
     brew install reattach-to-user-namespace
     ln -s ~/dotfiles/hammerspoon ~/.hammerspoon
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux"  ]; then
@@ -19,6 +17,11 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux"  ]; then
     sudo apt-get install zsh
     sudo apt-get install tmux
     sudo apt-get install vim-gnome
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository ppa:neovim-ppa/unstable
+    sudo apt-get update
+    sudo apt-get install neovim
+    sudo apt-get install python-dev python-pip python3-dev python3-pip
     git clone https://github.com/Anthony25/gnome-terminal-colors-solarized ~/gnome-terminal-colors-solarized
     ~/gnome-terminal-colors-solarized/install.sh
 fi
@@ -29,6 +32,9 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-sh <(curl https://j.mp/spf13-vim3 -L)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +BundleInstall! +BundleClean +q
+pip3 install neovim
+pip3 install --upgrade neovim
+nvim +UpdateRemotePlugins +q
